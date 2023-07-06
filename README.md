42e2e3ce8406b49b6cf98f34c22ce8f54245eed1
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2023-07-06 15:13:43 +0300

    Added the core functionalities

---
+++ README.md
@@ -0,0 +1,29 @@
+# Mind Map Maker
+
+Mind Map Maker is a desktop-based application that enables you to create, customize, and save your own mind maps. With a user-friendly interface and seamless functionality, it assists in generating structured and vivid mind maps for brainstorming, planning, and decision making.
+
+## Features
+
+- **Create Nodes**: Single click on the canvas to create a node. A prompt will appear for you to input the node's name.
+- **Customize Node Colors**: Pick your preferred color for the nodes, making your mind map more organized and visually engaging.
+- **Connect Nodes**: Nodes are automatically connected in their creation sequence, crafting an intuitive mind map structure.
+- **Delete Nodes**: Delete nodes effortlessly with a right-click, keeping your mind map neat and tidy.
+- **Save Mind Map**: Export your mind map as an image file (supports .jpg, .png, .bmp) for future reference, sharing, or printing.
+
+## Getting Started
+
+To run Mind Map Maker, you need to have Python installed on your machine. 
+
+1. Clone the repository to your local machine.
+2. Install the necessary Python packages: Tkinter and PIL.
+3. Run the `mindmap.py` script.
+
+Enjoy crafting your own mind maps!
+
+## Contributing
+
+We warmly welcome contributions! Feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the terms of the MIT License.
---
+++ mindmap.py
@@ -0,0 +1,101 @@
+from tkinter import *
+from tkinter import simpledialog, colorchooser
+from tkinter import messagebox
+from PIL import ImageGrab
+
+class Node:
+    def __init__(self, canvas, x, y, text, radius=20, color="blue"):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.text = text
+        self.color = color
+        self.radius = radius
+
+        self.node_id = self.canvas.create_oval(
+            x - radius, y - radius, x + radius, y + radius, fill=self.color
+        )
+        self.text_id = self.canvas.create_text(x, y, text=self.text, fill='white')
+
+    def move_to(self, x, y):
+        self.x = x
+        self.y = y
+
+        self.canvas.move(self.node_id, x - self.x, y - self.y)
+        self.canvas.move(self.text_id, x - self.x, y - self.y)
+
+class MindMap:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Mind Map Maker")
+        self.canvas = Canvas(self.master, width=800, height=600, bg='white')
+        self.canvas.pack()
+
+        self.node_color = "blue"
+        self.edge_color = "black"
+
+        self.nodes = []
+        self.previous_node = None
+
+        self.canvas.bind("<Button-1>", self.create_node)
+        self.canvas.bind("<Button-3>", self.delete_node)  # right click to delete node
+        self.canvas.bind("<B>", self.save_image)
+
+        # UI improvements
+        self.title_label = Label(self.master, text="Mind Map Maker", font=("Arial", 24))
+        self.title_label.pack(side=TOP)
+        
+        self.save_button = Button(self.master, text="Save Image", command=self.save_image)
+        self.save_button.pack(side=LEFT)
+
+        self.color_button = Button(self.master, text="Choose Node Color", command=self.choose_color)
+        self.color_button.pack(side=LEFT)
+
+    def create_node(self, event):
+        x, y = event.x, event.y
+        text = simpledialog.askstring("Input", "Enter node text:")
+        if text is None:
+            return
+        node = Node(self.canvas, x, y, text, color=self.node_color)
+        self.nodes.append(node)
+
+        if self.previous_node:
+            self.canvas.create_line(self.previous_node.x, self.previous_node.y, x, y, fill=self.edge_color)
+
+        self.previous_node = node
+
+    def delete_node(self, event):
+        x, y = event.x, event.y
+        item = self.canvas.find_closest(x, y)
+        for node in self.nodes:
+            if node.node_id == item[0] or node.text_id == item[0]:
+                self.canvas.delete(node.node_id)
+                self.canvas.delete(node.text_id)
+                self.nodes.remove(node)
+                break
+
+
+    def save_image(self):
+        x = self.master.winfo_rootx() + self.canvas.winfo_x()
+        y = self.master.winfo_rooty() + self.canvas.winfo_y()
+        x1 = x + self.canvas.winfo_width()
+        y1 = y + self.canvas.winfo_height()
+        img_path = simpledialog.askstring("Input", "Enter image path (include .jpg, .png, .bmp):")
+        if img_path is None:
+            return
+        # Check that img_path ends with a valid image file extension
+        if not (img_path.endswith('.jpg') or img_path.endswith('.png') or img_path.endswith('.bmp')):
+            messagebox.showerror("Error", "Invalid file extension. Please use .jpg, .png, or .bmp")
+            return
+        ImageGrab.grab().crop((x, y, x1, y1)).save(img_path)
+
+
+    def choose_color(self):
+        # Open color chooser dialog
+        color_code = colorchooser.askcolor(title="Choose color")
+        if color_code:
+            self.node_color = color_code[1]
+
+root = Tk()
+my_gui = MindMap(root)
+root.mainloop()
